1b75ec648ffe8e014128821612e1dca4893ace32
RemcoVeens <remco@track32.nl>
2026-04-26 14:38:15 +0200

    xor

---
+++ xor/main.go
@@ -0,0 +1,19 @@
+package main
+
+func crypt(plaintext, key []byte) []byte {
+	res := make([]byte, len(plaintext))
+	for i, c := range plaintext {
+		res[i] = c ^ key[i%len(key)]
+	}
+	return res
+}
+
+// encrypt function uses the crypt function for XOR encryption
+func encrypt(plaintext, key []byte) []byte {
+	return crypt(plaintext, key)
+}
+
+// decrypt function uses the crypt function for XOR decryption
+func decrypt(ciphertext, key []byte) []byte {
+	return crypt(ciphertext, key)
+}
